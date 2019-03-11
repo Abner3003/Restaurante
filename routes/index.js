9917334cbe4c7a03e833f53fd944ae553bce1823
abner cleim <abner@momentum.com.br>
2019-03-11 11:56:49 -0300

    ajustes dos includes

--- routes/index.js
+++ routes/index.js
@@ -1,54 +1,52 @@
 var conn = require('./../inc/db');
+var menus = require('../inc/menus');
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-conn.query(`
-          SELECT * FROM tb_menus ORDER BY title
-`,(err,results) =>{
+router.get('/', function (req, res, next) {
 
-    if(err){
-      console.log(err);
-    }
+  menus.getMenus().then(results);
+  res.render('index', {
+    title: 'Teste2',
+    menus: results
+  });
+});
 
-    res.render('index', { 
-      title: 'Teste2',
+router.get('/contacts', function (req, res, next) {
+  res.render('contacts', {
+    title: 'Entre em contato!',
+    background: 'images/img_bg_3.jpg',
+    h1: 'Diga um Oi'
+  })
+});
+
+router.get('/menus', function (req, res, next) {
+  menus.getMenus().then(results => {
+    res.render('menus', {
+      title: 'Nosso Menu',
+      background: 'images/img_bg_1.jpg',
+      h1: 'Saboreie nosso menu',
       menus: results
     });
   });
 });
 
-router.get('/contacts', function(req, res, next){
-  res.render('contacts',{
-    title:'Entre em contato!',
-    background:'images/img_bg_3.jpg',
-    h1:'Diga um Oi'
+
+
+router.get('/reservations', function (req, res, next) {
+  res.render('reservations', {
+    title: 'Faça sua Reserva',
+    background: 'images/img_bg_2.jpg',
+    h1: 'Faça a sua reserva!'
   })
 });
 
-router.get('/menus', function(req, res, next){
-  res.render('menus',{
-    title:'Nosso Menu',
-    background:'images/img_bg_1.jpg',
-    h1:'Saboreie nosso menu'
-  })
-});
-
-
-router.get('/reservations', function(req, res, next){
-  res.render('reservations',{
-    title:'Faça sua Reserva',
-    background:'images/img_bg_2.jpg',
-    h1:'Faça a sua reserva!'
-  })
-});
-
-router.get('/services', function(req, res, next){
-  res.render('services',{
-    title:'Nossos Serviços',
-    background:'images/img_bg_1.jpg',
-    h1:'É um prazer servi-los'
+router.get('/services', function (req, res, next) {
+  res.render('services', {
+    title: 'Nossos Serviços',
+    background: 'images/img_bg_1.jpg',
+    h1: 'É um prazer servi-los'
   })
 });
 module.exports = router;
